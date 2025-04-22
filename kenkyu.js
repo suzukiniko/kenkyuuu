5f6fc132413f9c275554723564cc549889a74456
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2025-04-22 04:19:22 +0000

    youwakaran

--- kenkyu.js
+++ kenkyu.js
@@ -1,17 +1,20 @@
 //kenkyuuuuuuuuuuuuu
+window.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("excanvas");
+  const ctx = canvas.getContext("2d");
 
-var canvas = document.getElementById("excanvas");
-var ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-function drawing(){
-  ctx.fillStyle = "rgba(35,35,255)";
-  ctx.fillRect(40,80,100,200);
-}
+  function drawing(){
+    ctx.fillStyle = "rgba(35,35,255)";
+    ctx.fillRect(40,80,100,200);
+  }
 
-var form = document.getElementById("pushed");
-form.addEventListener("submit",function(event){
-  event.preventDefault();
-  drawing();
-})
+
+  const form = document.getElementById("pushed");
+  form.addEventListener("submit",function(event){
+    event.preventDefault();
+    drawing();
+  });
+});
