9d2de6873bf8bba1676337187c999b75c2651c94
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2025-04-28 01:01:13 +0000

    js

--- kenkyu.js
+++ kenkyu.js
@@ -1,4 +1,5 @@
 //kenkyuuuuuuuuuuuuu
+
 window.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("excanvas");
   const ctx = canvas.getContext("2d");
@@ -11,26 +12,24 @@ window.addEventListener("DOMContentLoaded", function () {
     ctx.fillRect(100,400,300,300);
   }
 
-
   const form = document.getElementById("pushed");
+
   form.addEventListener("submit",function(event){
     event.preventDefault();
-
     mode("sta");
     drawing();
   });
-});
 
-function mode(state){
-  if(state == "fir"){
-    document.getElementById("kenkyu").innerHTML = "KENKYU";
-    document.getElementById("jishintokihaku").innerHTML = "苦しき稽古を積みし身が如何して負けよう自身と気迫";
-    document.getElementById("thatDay").innerHTML = "その日は必ず来る";
-  }else if(state == "sta"){
-    document.getElementById("kenkyu").innerHTML = "code moved";
-    document.getElementById("jishintokihaku").innerHTML = "";
-    document.getElementById("thatDay").innerHTML = "";
+  function mode(state){
+    if(state === "fir"){
+      document.getElementById("kenkyu").innerHTML = "KENKYU";
+      document.getElementById("jishintokihaku").innerHTML = "苦しき稽古を積みし身が如何して負けよう自身と気迫";
+      document.getElementById("thatDay").innerHTML = "その日は必ず来る";
+    }else if(state === "sta"){
+      document.getElementById("kenkyu").innerHTML = "code moved";
+      document.getElementById("jishintokihaku").innerHTML = "";
+      document.getElementById("thatDay").innerHTML = "";
+    }
   }
-
   mode(fir);
-}
+});
