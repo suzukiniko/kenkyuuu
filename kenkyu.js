7ec3d3abbd707ce20be0ef175ea3781650ad25ae
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2025-05-02 07:20:33 +0000

    state jotai

--- kenkyu.js
+++ kenkyu.js
@@ -52,31 +52,31 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 
   function mode(state){
-    if(state === "fir"){
+    if(jotai === "fir"){
       document.getElementById("maizuru").innerHTML = "home";
       document.getElementById("FamousSaying").innerHTML = "苦しき稽古を積みし身が如何して負けよう自身と気迫";
       document.getElementById("jikotosui").innerHTML = "その日は必ず来る";
       document.getElementById("pushed").style.display = "block";
       jotai = "fir";
-    }else if(state === "sta"){
+    }else if(jotai === "sta"){
       document.getElementById("maizuru").innerHTML = "modeSelect";
       document.getElementById("FamousSaying").innerHTML = "セルフ・マネジメント";
       document.getElementById("jikotosui").innerHTML = "";
       document.getElementById("pushed").style.display = "none";
       jotai = "sta";
-    }else if(state === "starting"){
+    }else if(jotai === "starting"){
       document.getElementById("maizuru").innerHTML = "starting";
       document.getElementById("FamousSaying").innerHTML = "始めるよ"
       document.getElementById("jikotosui").innerHTML = ""
       document.getElementById("pushed").style.display = "none"
       jotai = "starting";
-    }else if(state === "moding"){
+    }else if(jotai === "moding"){
       document.getElementById("maizuru").innerHTML = "moding";
       document.getElementById("FamousSaying").innerHTML = "モードを選んでね"
       document.getElementById("jikotosui").innerHTML = ""
       document.getElementById("pushed").style.display = "none"
       jotai = "moding";
-    }else if(state === "selecting"){
+    }else if(jotai === "selecting"){
       document.getElementById("maizuru").innerHTML = "selecting";
       document.getElementById("FamousSaying").innerHTML = "選択してね"
       document.getElementById("jikotosui").innerHTML = ""
