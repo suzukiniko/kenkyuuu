d2426ff0c9a0d3a3b36ecc3e41a3fb616501c3ca
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2025-05-22 16:16:25 +0000

    ボタンに応じて出現-pre

--- kenkyu.js
+++ kenkyu.js
@@ -6,10 +6,6 @@ window.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("excanvas");
   const ctx = canvas.getContext("2d");
 
-  let jotai = "fir";
-
-  mode(jotai);
-
   function resizeCanvas(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -19,12 +15,20 @@ window.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("resize",resizeCanvas);
 
+  document.getElementById("gen").style.display = "none";
+  document.getElementById("kob").style.display = "none";
+  document.getElementById("kan").style.display = "none";
+
   const form = document.getElementById("pushed");
+
+  document.getElementById("japanese").addEventListener("click",function(event){
+    document.getElementById("gen").style.display = "block";
+    document.getElementById("kob").style.display = "block";
+    document.getElementById("kan").style.display = "block";
+  });
 
   document.getElementById("start").addEventListener("click",function(event){
     event.preventDefault();
     jotai = "starting";
-    mode(jotai);
-    drawing();
   });
 });
